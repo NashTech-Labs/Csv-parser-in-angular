005aa794ccd3ef721e827fc4a6342cb0ece4cf73
kiran-0407 <kiran.kaur@nashtechglobal.com>
2025-01-28 20:46:28 +0530

    csv parser added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csvParser';
+  parsedData: any;
+
+  constructor(private papa: Papa) {
+  }
+
+  uploadFile(event: any){
+    this.papa.parse(event.target.files[0],{
+      complete: (result: any) => {
+          console.log('Parsed data results: ', result);
+          this.parsedData = result.data;
+      }
+    });
+  }
 }
